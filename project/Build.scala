0a97905bfb5bb0b976bb4c70cb62690157d8a838
xavier.carpentier <x.carpentier@greenivory.com>
2013-01-16 09:19:03 +0100

    change version

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,15 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "code-story"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName = "code-story"
+  val appVersion = "1.0"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    // Add your own project settings here
+  )
 
 }
